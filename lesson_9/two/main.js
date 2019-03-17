8486a5c37b2e2bc86ca03e8292abb24c9d11a49f
Serhiy Starokozhev <s.v.starokozhev@gmail.com>
2019-03-17 14:46:39 +0200

    lesson 9 -> task two, fix name

--- lesson_9/two/main.js
+++ lesson_9/two/main.js
@@ -4,4 +4,4 @@ function showUser(surname, name) {
     alert('Пользователь: ' + surname + ' ' + name + ', его возраст ' + this.value);
 }
 
-showUser.apply(age,['Serhiy', 'Starokozhev']);
+showUser.apply(age,['Starokozhev', 'Serhiy']);
